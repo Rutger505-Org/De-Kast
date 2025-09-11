a6fb5193cba767a9430efe0e1d07548621d36be0
Rutger Pronk <rutger.pronk11@gmail.com>
2025-09-11 11:13:48 +0200

    Hash password with better-auth instance

--- seed-users.ts
+++ seed-users.ts
@@ -1,77 +1,78 @@
+import { auth } from "@/server/auth";
 import { db } from "@/server/db";
 import { account, user } from "@/server/db/schema";
-import bcrypt from "bcryptjs";
 import { randomUUID } from "crypto";
 
 async function seed() {
-  const userId = randomUUID();
-
-  // Maybe hardcoded id to prevent double seed?
-
-  // hash the password with bcrypt
   const plainPassword = "secret";
-  const hashedPassword = await bcrypt.hash(plainPassword, 10);
 
-  const users = [
-    {
-      id: 1,
-      name: "One time",
-      email: "onetime@example.com",
-    },
-    {
-      id: 2,
-      name: "One time with Addon",
-      email: "onetimewithaddon@example.com",
-    },
-    {
-      id: 3,
-      name: "Two time",
-      email: "twotime@example.com",
-    },
-    {
-      id: 4,
-      name: "Two time with Addon",
-      email: "twotimewithaddon@example.com",
-    },
-    {
-      id: 5,
-      name: "Unlimited",
-      email: "unlimited@example.com",
-    },
-    {
-      id: 6,
-      name: "Unlimited with addon",
-      email: "unlimitedwithaddon@example.com",
-    },
-  ];
+  const authHelpers = await auth.$context;
+  const hashedPassword = await authHelpers.password.hash(plainPassword);
 
-  // create the user
-  await db.insert(user).values({
-    id: userId,
-    name: "One time",
-    email: "alice@example.com",
-    emailVerified: true,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  });
+  await Promise.all(
+    [
+      {
+        id: "1",
+        name: "One time",
+        email: "onetime@example.com",
+      },
+      {
+        id: "2",
+        name: "One time with Addon",
+        email: "onetimewithaddon@example.com",
+      },
+      {
+        id: "3",
+        name: "Two time",
+        email: "twotime@example.com",
+      },
+      {
+        id: "4",
+        name: "Two time with Addon",
+        email: "twotimewithaddon@example.com",
+      },
+      {
+        id: "5",
+        name: "Unlimited",
+        email: "unlimited@example.com",
+      },
+      {
+        id: "6",
+        name: "Unlimited with addon",
+        email: "unlimitedwithaddon@example.com",
+      },
+    ].map(async (u) => {
+      await db.insert(user).values({
+        ...u,
+        emailVerified: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
 
-  // link an account with hashed password
-  await db.insert(account).values({
-    id: randomUUID(),
-    accountId: "alice-local",
-    providerId: "credentials", // must match how you configured better-auth
-    userId,
-    password: hashedPassword,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  });
+      await db.insert(account).values({
+        id: randomUUID(),
+        accountId: `${u.email}-local`,
+        providerId: "credentials",
+        userId: u.id,
+        password: hashedPassword,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
 
-  console.log(
-    `✅ User seeded: alice@example.com with password "${plainPassword}"`,
+      console.log(
+        `✅ User seeded: ${u.name} (${u.email}) with password "${plainPassword}"`,
+      );
+    }),
   );
 }
 
-seed().then(() => {
-  console.log("🌱 Seeding complete");
-  process.exit(0);
-});
+seed()
+  .then(() => {
+    console.log("🌱 Seeding complete");
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error("❌ Seeding failed:", e);
+    console.log("Was the database already seeded?");
+    process.exit(1);
+  });
