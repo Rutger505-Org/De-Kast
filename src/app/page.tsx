8988a4f0e93b3d7aa9baa3a2e27f2f1bcc0037a6
Rutger Pronk <rutger.pronk11@gmail.com>
2025-09-12 12:29:35 +0200

    Add redirect when signed in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-"use client";
-
 import SignInForm from "@/app/_components/sign-in-form";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { auth } from "@/server/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function SignInPage() {
+export default async function SignInPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session) {
+    return redirect("/dashboard");
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-20">
       <h1 className={"text-4xl font-semibold"}>De Kast</h1>
