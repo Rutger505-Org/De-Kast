c283edba51ca4d331e5ed6f8840e960de649de7c
A <1207074@student.roc-nijmegen.nl>
2025-09-12 12:11:58 +0200

    Add cursus page with cursus component

--- src/app/dashboard/inschrijven/page.tsx
+++ src/app/dashboard/inschrijven/page.tsx
@@ -1,19 +1,46 @@
 import { auth } from "@/server/auth";
 import { headers } from "next/headers";
+import Link from "next/link";
+import Cursus from "../../_components/cursus";
 
 export default async function Home() {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
+  const cursusses = [
+    { name: "Yoga", datum: "23-4-25", duur: "14.00 - 15.00" },
+    { name: "Pilates", datum: "12-6-25", duur: "13.00 - 14.00" },
+    { name: "Paaldansen", datum: "1-9-26", duur: "18.00 - 21.00" },
+  ];
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <main className={"flex flex-col items-center justify-center gap-10"}>
         <div className={"flex flex-col items-center space-y-2.5"}>
-          <h1 className="text-4xl font-bold">Cursus</h1>
+          <h1 className="text-4xl font-bold">Cursus toevoegen</h1>
           <h2 className={"max-w-md text-center text-3xl font-bold"}>
             {session?.user.name}
           </h2>
+        </div>
+        {cursusses.map((cursus) => {
+          return (
+            <Cursus
+              name={cursus.name}
+              datum={cursus.datum}
+              duur={cursus.duur}
+              key={cursus.name}
+            />
+          );
+        })}
+
+        <div className="absolute bottom-10 left-20">
+          <Link
+            href="/dashboard"
+            className="border-2 border-solid border-green-500 px-6 py-1"
+          >
+            Terug
+          </Link>
         </div>
       </main>
     </div>
